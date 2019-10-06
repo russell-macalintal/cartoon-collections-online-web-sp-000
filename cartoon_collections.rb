2c73d866f392881451a0429c88d20e67f2b0af13
Russell Macalintal <russell.macalintal@gmail.com>
2019-10-06 02:56:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,3 @@
-require 'pry'
 def roll_call_dwarves(array)# code an argument here
   # Your code here
   array.each_with_index {|name, index| puts "#{index + 1} #{name}"}
@@ -7,7 +6,6 @@ end
 def summon_captain_planet(array)# code an argument here
   # Your code here
   array.map {|element| element.upcase + "!"}
-  binding.pry
 end
 
 def long_planeteer_calls# code an argument here
