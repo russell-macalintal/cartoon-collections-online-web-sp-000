8f4be314b14a890a15b7d7c911eb53900092092a
Russell Macalintal <russell.macalintal@gmail.com>
2019-10-06 03:01:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
-  array.collect {|element| element.upcase + "!"}
+  array.map {|element| element.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
